8eb98b71b41645a062f0b62515fd81cadbf80d78
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-10 15:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
+ 
+  var tr= document.createElement("tr");
+  tr.innerHTML= "<td> New Cell1</td> <td>New Cell2</td>";
+	document.getElementbyId(sampleTable).appendchild(tr);
+	
   
 }
